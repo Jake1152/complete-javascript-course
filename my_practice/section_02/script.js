da02f3d91b96b9b192c812a69095e7c38f2e0516
Jake1152 <rancho974@gmail.com>
2023-08-29 01:20:54 +0900

    [ADD] Type conversion and Type coercion

--- my_practice/section_02/script.js
+++ my_practice/section_02/script.js
@@ -268,29 +268,77 @@ console.log(typeof null);
 // multiple
 // lines`);
 
-const age = 15;
-const isOldEnough = age >= 18; // True
+// const age = 15;
+// const isOldEnough = age >= 18; // True
 
 /**
  * 컨트롤 구조라고 부른다
  */
 // if (age >= 18) {
-if (isOldEnough) {
-    console.log("Marie can start driving license");
-} else {
-    const yearsLeft = 18 - age;
-    console.log(`Marie can not start driving license TT
-    ${yearsLeft} years:)`);
-}
+// if (isOldEnough) {
+//     console.log("Marie can start driving license");
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Marie can not start driving license TT
+//     ${yearsLeft} years:)`);
+// }
+
+// let century;
+// const birthYear = 1980;
+// if (birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
+// console.log(century)
 
 /**
- * 
+ * 타입 변환
+ * 명시, 묵시 두가지
+ * 변환하고자 할때
  */
-let century;
-const birthYear = 1980;
-if (birthYear <= 2000) {
-    century = 20;
-} else {
-    century = 21;
-}
-console.log(century)
+// #1
+// const inputYear = '1991'
+// console.log(inputYear + 18); // 199118
+
+// #2
+const inputYear = '1991'
+console.log(Number(inputYear), inputYear); // 1991
+console.log(Number(inputYear) + 18); // 199118
+
+console.log(Number("jake")); // 199118
+
+console.log(typeof NaN); // 199118
+
+console.log(String(23), 23);
+
+
+// type conversion
+// type crrection
+// 숫자지면 타입강제 변환으로 문제 없이 된다.
+// 템플릿 리터럴도 동일
+console.log('I am ' + 23 + ' years old');
+console.log('23' + '10' + 3); // 23103
+/**
+ * 왜 숫자가 되었는ㄷ가
+ * 마이너스 연산자가 촉발함
+ * +는 문자열에서 다르게 해석 될수 있는 듯
+ * append으 느낌
+ */
+console.log('23' - '10' - 3); // 10
+console.log('24' / '3');
+
+
+let n = '1' + 1; // '11'
+n = n - 1; // 11 - 1
+console.log(n) // 10
+
+console.log(2+3+4+'5'); // "95"
+/*
+2+3+4 = 9
+9 + '5' => '95'
+*/
+
+console.log('10'-'4'-'3'-2+'5'); // "15"
+console.log('10'*'4'); // 40
+
