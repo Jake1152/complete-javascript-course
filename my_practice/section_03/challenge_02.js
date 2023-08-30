ba9ab99fe2b8bb09d0757e1b79ffad454f35f6b3
Jake1152 <rancho974@gmail.com>
2023-08-31 02:42:25 +0900

    [FIX] add lecture example

--- my_practice/section_03/challenge_02.js
+++ my_practice/section_03/challenge_02.js
@@ -9,3 +9,15 @@ const calcTip = (bill) => {
 const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[bills.length - 1])];
 console.log(tips);
+
+/**
+ * Lecture
+ */
+
+const calcTip1 = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills0 = [125, 555, 44];
+const tips0 = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[bills.length - 1])];
+console.log(tips);
