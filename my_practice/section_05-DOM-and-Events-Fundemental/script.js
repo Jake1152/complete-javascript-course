932c9de8cf4a56e3d1ce70cfe506d9e63540dac4
Jake1152 <rancho974@gmail.com>
2023-09-06 04:51:32 +0900

    [ADD] Taste DOM

---
+++ my_practice/section_05-DOM-and-Events-Fundemental/script.js
@@ -0,0 +1,19 @@
+/**
+ * 내 번호를 맞추어라 게임
+
+번호 넣고 맞추기 
+기본 점수가 주어지고 맞추면 증가 틀리면 감소
+맞추면 배경도 변경
+again버튼을 통해 리셋가능
+
+깃에서 스타터 가져옴
+
+*/
+
+'use strict';
+
+console.log(document.querySelector('.message'));
+console.log(document.querySelector('.message').textContent);
+
+
+
