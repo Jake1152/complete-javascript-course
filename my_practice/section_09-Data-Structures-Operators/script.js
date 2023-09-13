b121fa1c01cc79a20d7b807d587a2cfe1f5381c2
Jake1152 <rancho974@gmail.com>
2023-09-14 03:11:25 +0900

    [ADD] Map

--- my_practice/section_09-Data-Structures-Operators/script.js
+++ my_practice/section_09-Data-Structures-Operators/script.js
@@ -61,43 +61,75 @@ const restaurant = {
 };
 
 /**
+ * Map
+ * key, value
+ */
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Fireze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :)')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+// rest.delete(2);
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+/**
  * Set
  */
+// const ordersSet = new Set([
+//   'Pasta',
+//   'Pizza',
+//   'Pizza',
+//   'Risotto',
+//   'Pasta',
+//   'Pizza',
+// ]);
 
-const ordersSet = new Set([
-  'Pasta',
-  'Pizza',
-  'Pizza',
-  'Risotto',
-  'Pasta',
-  'Pizza',
-]);
-
-console.log(ordersSet);
-
-console.log(new Set('Jonas')); // Set(5) { 'J', 'o', 'n', 'a', 's' }
-
-console.log(ordersSet.size); // 3
-console.log(ordersSet.has('Pizza')); // 3
-console.log(ordersSet.has('Bread')); // 3
-ordersSet.add('Garlic Bread');
-ordersSet.add('Garlic Bread');
-ordersSet.delete('Risotto');
-console.log(ordersSet);
-// console.log(ordersSet(2)); // no index, make error
-// ordersSet.clear();
 // console.log(ordersSet);
 
-// 순서 상관 없음
-for (const order of ordersSet) console.log(order);
+// console.log(new Set('Jonas')); // Set(5) { 'J', 'o', 'n', 'a', 's' }
 
-const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
-// 각각의 element들이 array에 들어가게됨
-const staffUnique = [...new Set(staff)];
-// const staffUnique = new Set(staff);
-console.log(staffUnique);
+// console.log(ordersSet.size); // 3
+// console.log(ordersSet.has('Pizza')); // 3
+// console.log(ordersSet.has('Bread')); // 3
+// ordersSet.add('Garlic Bread');
+// ordersSet.add('Garlic Bread');
+// ordersSet.delete('Risotto');
+// console.log(ordersSet);
+// // console.log(ordersSet(2)); // no index, make error
+// // ordersSet.clear();
+// // console.log(ordersSet);
 
-console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']));
+// // 순서 상관 없음
+// for (const order of ordersSet) console.log(order);
+
+// const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+// // 각각의 element들이 array에 들어가게됨
+// const staffUnique = [...new Set(staff)];
+// // const staffUnique = new Set(staff);
+// console.log(staffUnique);
+
+// console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']));
 
 /**
  *  Looping Objects: Object Keys, Values, and Entries
