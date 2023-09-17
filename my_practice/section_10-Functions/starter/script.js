dc3d4c03d1e4bb963edf6313541d81b328dc1720
Jake1152 <rancho974@gmail.com>
2023-09-18 07:09:06 +0900

    [ADD] high order function

--- my_practice/section_10-Functions/starter/script.js
+++ my_practice/section_10-Functions/starter/script.js
@@ -36,35 +36,79 @@
  * passing arguments
  */
 
-const flight = 'LH234';
-const jonas = {
-  name: 'jonas schemd',
-  passport: 2479456789,
-};
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'jonas schemd',
+//   passport: 2479456789,
+// };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
 
-  if (passenger.passport === 2479456789) {
-    alert('Check in');
-  } else {
-    alert('Wrong passport!');
-  }
-};
+//   if (passenger.passport === 2479456789) {
+//     alert('Check in');
+//   } else {
+//     alert('Wrong passport!');
+//   }
+// };
 
+// // checkIn(flight, jonas);
+// // console.log(flight);
+// // console.log(jonas);
+
+// // // Is the name as doing
+// // const flightNum = flight;
+// // const passenger = jonas;
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() + 10000000);
+// };
+
+// // passport 변경
+// newPassport(jonas);
 // checkIn(flight, jonas);
-// console.log(flight);
-// console.log(jonas);
 
-// // Is the name as doing
-// const flightNum = flight;
-// const passenger = jonas;
-
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() + 10000000);
+/**
+ * Functions Accepting Callback Functions
+ * 다른 함수를 입려그올 받아들이는 함수를 생성
+ */
+// 간단한 문자열 반환하는 함수
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
 };
 
-// passport 변경
-newPassport(jonas);
-checkIn(flight, jonas);
+const upperFirstWord = function (str) {
+  const [first, ...othres] = str.split(' ');
+  return [first.toUpperCase(), ...othres.join(' ')];
+};
+
+// Higher-order function
+// console.log(oneWord('Test 55'));
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+// 두번쨰 인자로 전달한 함수를 호출한다, 콜백으로 사용
+/**
+ * 콜백 함수는 fn이라고 불리운다
+ */
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callback all the time
+const high5 = function () {
+  console.log('hi');
+};
+document.body.addEventListener('click', high5);
+
+// forEach에도 콜백함수 사용가능
+['Jonas', 'Matias', 'Adam'].forEach(high5);
+
+/**
+ * 위에 transform함수를 씀으로써 추상화를 이뤄낸다
+ * 추상화를 함으로써 세부사항들은 숨길 수 있다
+ */
