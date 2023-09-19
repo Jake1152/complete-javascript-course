9665879a9d0894a8a4c43f0af519cc8c3c0d687e
Jake1152 <rancho974@gmail.com>
2023-09-20 07:53:11 +0900

    [ADD] forEach vs for .. of ...

--- my_practice/section_11-Arrays-Bankist/script.js
+++ my_practice/section_11-Arrays-Bankist/script.js
@@ -36,30 +36,30 @@ const account4 = {
 const accounts = [account1, account2, account3, account4];
 
 // Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
+// const labelWelcome = document.querySelector('.welcome');
+// const labelDate = document.querySelector('.date');
+// const labelBalance = document.querySelector('.balance__value');
+// const labelSumIn = document.querySelector('.summary__value--in');
+// const labelSumOut = document.querySelector('.summary__value--out');
+// const labelSumInterest = document.querySelector('.summary__value--interest');
+// const labelTimer = document.querySelector('.timer');
 
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
+// const containerApp = document.querySelector('.app');
+// const containerMovements = document.querySelector('.movements');
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+// const btnLogin = document.querySelector('.login__btn');
+// const btnTransfer = document.querySelector('.form__btn--transfer');
+// const btnLoan = document.querySelector('.form__btn--loan');
+// const btnClose = document.querySelector('.form__btn--close');
+// const btnSort = document.querySelector('.btn--sort');
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+// const inputLoginUsername = document.querySelector('.login__input--user');
+// const inputLoginPin = document.querySelector('.login__input--pin');
+// const inputTransferTo = document.querySelector('.form__input--to');
+// const inputTransferAmount = document.querySelector('.form__input--amount');
+// const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+// const inputCloseUsername = document.querySelector('.form__input--user');
+// const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -114,13 +114,43 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
  * The new at Method
  * 배열의 끝요소를 가져오고 싶거나 끝요소부터 세고 싶다면 at()을 쓰는게 좋다
  */
-const arr = [23, 11, 64];
-console.log(arr[0]); // 23
-console.log(arr.at(0)); // 23
-console.log(arr[42]); // undefined
-console.log(arr.at(42)); // undefined
+// const arr = [23, 11, 64];
+// console.log(arr[0]); // 23
+// console.log(arr.at(0)); // 23
+// console.log(arr[42]); // undefined
+// console.log(arr.at(42)); // undefined
 
-console.log(arr[arr.length - 1]); // 64
-console.log(arr.slice(-1)[0]); // 64
-console.log(arr.at(-1)); // 64
-console.log(arr[-1]); // undefined
+// console.log(arr[arr.length - 1]); // 64
+// console.log(arr.slice(-1)[0]); // 64
+// console.log(arr.at(-1)); // 64
+// console.log(arr[-1]); // undefined
+
+/**
+ * Looping Arrays: forEach
+ */
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1} : You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1} : You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('\n---- FOREACH ----');
+// movements.forEach(function (movement) {
+// for of와 매개변수 순서가 다름에 주의해야한다
+// forEach항상 전체 배열에 걸려 반복한다.
+// for of는 break로 중간에 나올 수 있다.
+movements.forEach(function (movement, index, array) {
+  if (movement > 0) {
+    console.log(`Movement ${index + 1} : You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1} : You withdrew ${Math.abs(movement)}`);
+  }
+});
+// 0: function(200)
+// 0: function(450)
+// 0: function(400)
