fb69dffa52f3e11880bf5bffae51656c4039dd60
Jake1152 <rancho974@gmail.com>
2023-09-20 07:40:16 +0900

    [ADD] at() method

--- my_practice/section_11-Arrays-Bankist/script.js
+++ my_practice/section_11-Arrays-Bankist/script.js
@@ -75,37 +75,52 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
+// let arr = ['a', 'b', 'c', 'd', 'e'];
 
-// slice method
-console.log(arr.slice(2)); //["c", "d", "e"]
-console.log(arr.slice(2, 4)); //["c", "d"]
-console.log(arr.slice(-2)); // ['d', 'e']
-console.log(arr.slice(-1)); // ['e']
-console.log(arr.slice(1, -2)); // ['b', 'c']
-console.log(arr.slice()); // ['a', 'b', 'c', 'd', 'e']
-console.log([...arr]); // ['a', 'b', 'c', 'd', 'e']
+// // slice method
+// console.log(arr.slice(2)); //["c", "d", "e"]
+// console.log(arr.slice(2, 4)); //["c", "d"]
+// console.log(arr.slice(-2)); // ['d', 'e']
+// console.log(arr.slice(-1)); // ['e']
+// console.log(arr.slice(1, -2)); // ['b', 'c']
+// console.log(arr.slice()); // ['a', 'b', 'c', 'd', 'e']
+// console.log([...arr]); // ['a', 'b', 'c', 'd', 'e']
 
-// SPLICE
-/*
-원래 배열을 바꿈
-*/
-// console.log(arr.splice(2)); // ['c', 'd', 'e'] => ['a', 'b']
-arr.splice(-1); //
-console.log(arr); //['a', 'b', 'c', 'd']
-arr.splice(1, 2); //
-console.log(arr); //['a', 'd']
+// // SPLICE
+// /*
+// 원래 배열을 바꿈
+// */
+// // console.log(arr.splice(2)); // ['c', 'd', 'e'] => ['a', 'b']
+// arr.splice(-1); //
+// console.log(arr); //['a', 'b', 'c', 'd']
+// arr.splice(1, 2); //
+// console.log(arr); //['a', 'd']
 
-// REVERSE
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse);
-console.log(arr2);
+// // REVERSE
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse);
+// console.log(arr2);
 
-// CONCAT
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
+// // CONCAT
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
 
-// JOIN
-console.log(letters.join(' - ')); // a - b - c - d - e - j - i - h - g - f
+// // JOIN
+// console.log(letters.join(' - ')); // a - b - c - d - e - j - i - h - g - f
+
+/**
+ * The new at Method
+ * 배열의 끝요소를 가져오고 싶거나 끝요소부터 세고 싶다면 at()을 쓰는게 좋다
+ */
+const arr = [23, 11, 64];
+console.log(arr[0]); // 23
+console.log(arr.at(0)); // 23
+console.log(arr[42]); // undefined
+console.log(arr.at(42)); // undefined
+
+console.log(arr[arr.length - 1]); // 64
+console.log(arr.slice(-1)[0]); // 64
+console.log(arr.at(-1)); // 64
+console.log(arr[-1]); // undefined
