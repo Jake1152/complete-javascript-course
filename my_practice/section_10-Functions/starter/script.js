54ac4ba584a0b37082c95d9f00101921ed7898cf
Jake1152 <rancho974@gmail.com>
2024-05-24 08:32:40 +0900

    [ADD] 129. Default Parameters

--- my_practice/section_10-Functions/starter/script.js
+++ my_practice/section_10-Functions/starter/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+/** # 129. Default Parameters
+ */
+const bookings = [];
+
+// const alines
+const createBooking = function (
+  flightNum = 123,
+  numPassengers = 42,
+  price = 1000
+) {
+  /** Before ES6 version default parameter
+   * numPassengers = numPassengers || 1;
+   * price = price || 199;
+   */
+
+  // common code
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  // console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking();
+console.log(`bookings : `, bookings);
+
+createBooking('LH23');
+console.log(`bookings : `, bookings);
+
+createBooking('LH23', 800);
+console.log(`bookings : `, bookings);
+
+createBooking('LH23', 800, 420000);
+console.log(`bookings : `, bookings);
+
+createBooking('LH23', undefined, 420000);
