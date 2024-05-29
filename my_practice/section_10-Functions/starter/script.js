51a6b2d67588c5818d896a366b23791b39e27b2a
Jake1152 <rancho974@gmail.com>
2024-05-30 08:56:17 +0900

    [ADD] 134. The call and apply Methods

--- my_practice/section_10-Functions/starter/script.js
+++ my_practice/section_10-Functions/starter/script.js
@@ -80,7 +80,7 @@
 // // pass reference
 // checkIn(flight, jonas); // wrong passport, 얉은 복사로 인해 newPassport()함수를 거치며 값이 변경됨
 
-/* 132. Functions Accepting Callback Functions
+/* # 132. Functions Accepting Callback Functions
  */
 
 // const oneWord = function (str) {
@@ -111,28 +111,92 @@
 // document.body.addEventListener('click', high5);
 // ['Jonas', 'Martha', 'Adam'].forEach(high5);
 
-/* 133. Functinos Returning Functions
+/* # 133. Functinos Returning Functions
  */
 
 // scope chain을 쓰게끔 만든다.
-const greet = function (greeting) {
-  // greeting: greeting,
-  return function (name) {
-    console.log(`${greeting} ${name}`);
-  };
+// const greet = function (greeting) {
+//   // greeting: greeting,
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greeterHey = greet('Hey');
+// greeterHey('Jonas');
+// greeterHey('Steven');
+
+// greet('Hello')('Jonas');
+
+// // 다른 함수를 반환 함수로 갖는다는 것의 의미
+// // const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
+// const greetArrow = greeting => {
+//   return name => {
+//     return console.log(`${greeting} ${name}`);
+//   };
+// };
+// greetArrow('Hello')('Jonas');
+
+/* # 134. The call and apply Methods
+ */
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function() // old version
+  // new version
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
 };
 
-const greeterHey = greet('Hey');
-greeterHey('Jonas');
-greeterHey('Steven');
+lufthansa.book(742, 'Jake');
+lufthansa.book(954, 'Jim Carrey');
+console.log(lufthansa);
 
-greet('Hello')('Jonas');
-
-// 다른 함수를 반환 함수로 갖는다는 것의 의미
-// const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
-const greetArrow = greeting => {
-  return name => {
-    return console.log(`${greeting} ${name}`);
-  };
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
 };
-greetArrow('Hello')('Jonas');
+
+const book = lufthansa.book;
+
+// ! Does not work
+// book(23, 'Sara William'); // error, airline이 없다고 나온다. 전역변수인 book 영역에서 airline 변수가 없기에 그러하다.
+
+// eurowings.book = lufthansa.book;
+// eurowings.book(23, 'Sara Williams'); // Work
+
+// fucntion은 object에 불가하다
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+// book.call(swiss, 583, 'Mary Cooper');
+// book.call(swiss, (583, 'Mary Cooper'));
+// console.log(swiss);
+
+// Apply method, call과의 차이점으로는 인수목록을 받지 않는다고 한다.
+// apply는  더 이상 모던JS에서 쓰이지 않는다. 더 나은 방법이 있다고 한다.
+
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData); // destructuring 가능
+console.log(swiss);
+
+// book.call(swiss, flightData); // destructuring이 안됨
+// console.log(swiss);
+
+book.call(swiss, ...flightData); // apply()처럼 destructuring 가능
+console.log(swiss);
