7df7ae0330aee443f3db6a601d570e1daef582f1
Jake1152 <rancho974@gmail.com>
2024-05-30 19:25:18 +0900

    [ADD] 135. bind

--- my_practice/section_10-Functions/starter/script.js
+++ my_practice/section_10-Functions/starter/script.js
@@ -153,9 +153,9 @@ const lufthansa = {
   },
 };
 
-lufthansa.book(742, 'Jake');
-lufthansa.book(954, 'Jim Carrey');
-console.log(lufthansa);
+// lufthansa.book(742, 'Jake');
+// lufthansa.book(954, 'Jim Carrey');
+// console.log(lufthansa);
 
 const eurowings = {
   airline: 'Eurowings',
@@ -172,11 +172,11 @@ const book = lufthansa.book;
 // eurowings.book(23, 'Sara Williams'); // Work
 
 // fucntion은 object에 불가하다
-book.call(eurowings, 23, 'Sarah Williams');
-console.log(eurowings);
+// book.call(eurowings, 23, 'Sarah Williams');
+// console.log(eurowings);
 
-book.call(lufthansa, 239, 'Mary Cooper');
-console.log(lufthansa);
+// book.call(lufthansa, 239, 'Mary Cooper');
+// console.log(lufthansa);
 
 const swiss = {
   airline: 'Swiss Air Lines',
@@ -191,12 +191,60 @@ const swiss = {
 // Apply method, call과의 차이점으로는 인수목록을 받지 않는다고 한다.
 // apply는  더 이상 모던JS에서 쓰이지 않는다. 더 나은 방법이 있다고 한다.
 
-const flightData = [583, 'George Cooper'];
-book.apply(swiss, flightData); // destructuring 가능
-console.log(swiss);
+// const flightData = [583, 'George Cooper'];
+// book.apply(swiss, flightData); // destructuring 가능
+// console.log(swiss);
 
 // book.call(swiss, flightData); // destructuring이 안됨
 // console.log(swiss);
 
-book.call(swiss, ...flightData); // apply()처럼 destructuring 가능
-console.log(swiss);
+// book.call(swiss, ...flightData); // apply()처럼 destructuring 가능
+// console.log(swiss);
+
+/**
+ * 135. bind
+ */
+const bookEW = book.bind(eurowings);
+const bookLF = book.bind(lufthansa);
+const bookSW = book.bind(swiss);
+bookEW(23, 'Eric');
+bookLF('123', 'Eric');
+bookSW('456', 'Eric');
+
+// https://en.wikipedia.org/wiki/Partial_application
+// 미리 사용할 인자 일부를 설정하는 방법 => Partial Application 기법
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jake');
+
+const bookEW23Jason = book.bind(eurowings, 23, 'Jason');
+bookEW23Jason();
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes); // event 처리 함수에에서는 this키워드가 event handler에 연결된 DOM 요소를 가리킨다. 여기서는 button요소를 가리킨다.
+};
+// document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane);
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.2;
+console.log(addVAT(100));
+console.log(addTax(0.23, 23));
+
+const addTaxRate = rate => {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
