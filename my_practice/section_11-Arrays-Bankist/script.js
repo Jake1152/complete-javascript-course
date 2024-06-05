ffd5a98ef17abbf030eb0d3d68741ae7e0b43b81
Jake1152 <rancho974@gmail.com>
2024-06-05 12:37:09 +0900

    [ADD] 143. Simple Array Methods

--- my_practice/section_11-Arrays-Bankist/script.js
+++ my_practice/section_11-Arrays-Bankist/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
@@ -59,5 +61,98 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+const currenies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound Sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
 /** # 143. Simple Array Methods
+ * array도 object이다.
  */
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+/** SLICE
+ * 원본을 바꾸지 않고 얕은 복사를 한다
+ */
+// const pieceOfArr = arr.slice(2, 4);
+// console.log(`pieceOfArr : `, pieceOfArr);
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2)); // negative begin parameter
+console.log(arr.slice(-1)); // last element of array
+console.log(arr.slice(1, -2));
+console.log(arr);
+console.log(arr.slice()); // shallow copy
+
+let arrSlice = arr.slice(); // shallow copy
+arrSlice[0] = '42';
+console.log('arrSlice', arrSlice); // ['42', 'b', 'c', 'd', 'e'];
+console.log(arr); // ['42', 'b', 'c', 'd', 'e'];
+
+// console.log([...arr]); // deep copy
+
+let arrSpread = [...arr];
+arrSpread[0] = '42';
+console.log('arrSpread', arrSpread);
+console.log(arr);
+
+/** SPLICE
+ * 원본에서 잘라감, 원본도 바뀐다.
+ */
+// console.log('#SPLICE ', arr);
+// console.log(arr.splice(-1, 1, 'z')); // []
+// console.log(arr); // ['a', 'b', 'c', 'd', 'z', 'e']
+// console.log(arr.splice(arr.length, 0, 'z')); // []
+// console.log(arr); // ['a', 'b', 'c', 'd', 'z', 'e']
+// console.log(arr.splice(2)); // ['c', 'd', 'e']
+// console.log(arr); // ['a', 'b']
+// console.log(arr.splice(-1)); // ['b']
+// console.log(arr); // ['a']
+
+// /** REVERSE
+//  * 거꾸로 뒤집음
+//  * 뒤집힌 배열을 반환값으로 주지만
+//  * 원본까지 뒤집는다.
+//  *
+//  * 실제상황에서 원본까지 바꾸는 것은 안되는 경우가 많다.
+//  */
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log('#REVERSE ', arr);
+// console.log(arr2.reverse()); // ['j', 'g', 'h', 'i', 'j']
+// console.log(arr2); // ['j', 'g', 'h', 'i', 'j']
+
+// /** CONCAT
+//  * 거꾸로 뒤집음
+//  * 뒤집힌 배열을 반환값으로 주지만
+//  * 원본까지 뒤집는다.
+//  */
+
+// console.log('#CONCAT ', arr);
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
+// console.log(arr);
+// console.log(arr2);
+
+// /** JOIN
+//  * 배열의 각 요소들을 join에 주어진값으로 연결한다
+//  */
+// console.log('#JOIN ', arr);
+// console.log(arr.join(arr, arr2)); // aa,b,c,d,eba,b,c,d,eca,b,c,d,eda,b,c,d,ee
+// console.log(arr.join(arr)); // aa,b,c,d,eba,b,c,d,eca,b,c,d,eda,b,c,d,ee
+// console.log(arr.join(arr2)); // af,g,h,i,jbf,g,h,i,jcf,g,h,i,jdf,g,h,i,je
+// console.log(arr.join(arr2, arr)); // af,g,h,i,jbf,g,h,i,jcf,g,h,i,jdf,g,h,i,je
+// console.log(arr.join(' - ')); // a - b - c - d - e
+// console.log(arr);
+// console.log(arr2);
